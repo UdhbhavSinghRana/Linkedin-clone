88d0944ba51a9514d8aa114661e733e1b46083e6
Udhbhav Singh Rana <21889udbhav@gmail.com>
2023-02-26 17:40:20 +0530

    Add Sidebar division

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
-
+import Sidebar from './Sidebar';
+import Content from './Content';
 function App() {
   return (
-    <div className='app'>
-      <div>
+    <div className=''>
+      <div className='flex justify-center'>
         <Header />
+      </div>
+      <div className='flex justify-center bg-zinc-100 h-full'>
+      <div className='flex justify-between  w-4/5 py-5'>
+        <div >
+          <Sidebar />
+        </div>
+        <div>
+           <Content />
+        </div>
+        <div>
+          end
+        </div>
+      </div>
       </div>
     </div>
   );
